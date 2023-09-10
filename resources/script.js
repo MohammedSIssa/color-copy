9d5785a764260dbb67ad085dadcd9df0a73cab82
MohammedSIssa <mohammedsissa1999@gmail.com>
2023-09-10 18:18:38 +0300

    Ver 1

---
+++ resources/script.js
@@ -0,0 +1,97 @@
+let color1 = document.querySelector('.col-1')
+color1.addEventListener("click",function(){
+    let color1Value = color1.querySelector('.color-1').textContent
+    navigator.clipboard.writeText(color1Value)
+   console.log(color1Value)
+})
+
+let color2 = document.querySelector('.col-2')
+color2.addEventListener("click",function(){
+    let color2Value = color2.querySelector('.color-2').textContent
+    navigator.clipboard.writeText(color2Value)
+   console.log(color2Value)
+})
+
+let color3 = document.querySelector('.col-3')
+color3.addEventListener("click",function(){
+    let color3Value = color3.querySelector('.color-3').textContent
+    navigator.clipboard.writeText(color3Value)
+   console.log(color3Value)
+})
+
+let color4 = document.querySelector('.col-4')
+color4.addEventListener("click",function(){
+    let color4Value = color4.querySelector('.color-4').textContent
+    navigator.clipboard.writeText(color4Value)
+   console.log(color4Value)
+})
+
+let color5 = document.querySelector('.col-5')
+color5.addEventListener("click",function(){
+    let color5Value = color5.querySelector('.color-5').textContent
+    navigator.clipboard.writeText(color5Value)
+   console.log(color5Value)
+})
+
+let color6 = document.querySelector('.col-6')
+color2.addEventListener("click",function(){
+    let color6Value = color6.querySelector('.color-6').textContent
+    navigator.clipboard.writeText(color6Value)
+   console.log(color6Value)
+})
+
+let color7 = document.querySelector('.col-7')
+color7.addEventListener("click",function(){
+    let color7Value = color7.querySelector('.color-7').textContent
+    navigator.clipboard.writeText(color7Value)
+   console.log(color7Value)
+})
+
+let color8 = document.querySelector('.col-8')
+color8.addEventListener("click",function(){
+    let color8Value = color8.querySelector('.color-8').textContent
+    navigator.clipboard.writeText(color8Value)
+   console.log(color8Value)
+})
+
+let color9 = document.querySelector('.col-9')
+color9.addEventListener("click",function(){
+    let color9Value = color9.querySelector('.color-9').textContent
+    navigator.clipboard.writeText(color9Value)
+   console.log(color9Value)
+})
+
+let color10 = document.querySelector('.col-10')
+color10.addEventListener("click",function(){
+    let color10Value = color10.querySelector('.color-10').textContent
+    navigator.clipboard.writeText(color10Value)
+   console.log(color10Value)
+})
+
+let color11 = document.querySelector('.col-11')
+color11.addEventListener("click",function(){
+    let color11Value = color11.querySelector('.color-11').textContent
+    navigator.clipboard.writeText(color11Value)
+   console.log(color11Value)
+})
+
+let color12 = document.querySelector('.col-12')
+color12.addEventListener("click",function(){
+    let color12Value = color12.querySelector('.color-12').textContent
+    navigator.clipboard.writeText(color12Value)
+   console.log(color12Value)
+})
+
+let color13 = document.querySelector('.col-13')
+color13.addEventListener("click",function(){
+    let color13Value = color13.querySelector('.color-13').textContent
+    navigator.clipboard.writeText(color13Value)
+   console.log(color13Value)
+})
+
+let color14 = document.querySelector('.col-14')
+color14.addEventListener("click",function(){
+    let color14Value = color14.querySelector('.color-14').textContent
+    navigator.clipboard.writeText(color14Value)
+   console.log(color14Value)
+})
