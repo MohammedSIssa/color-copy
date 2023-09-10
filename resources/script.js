dae92250e9747a527b6f30569066d7f4cc591f5b
MohammedSIssa <mohammedsissa1999@gmail.com>
2023-09-10 19:03:09 +0300

    Added more colors

--- resources/script.js
+++ resources/script.js
@@ -95,3 +95,202 @@ color14.addEventListener("click",function(){
     navigator.clipboard.writeText(color14Value)
    console.log(color14Value)
 })
+
+let color15 = document.querySelector('.col-15')
+color15.addEventListener("click",function(){
+    let color15Value = color15.querySelector('.color-15').textContent
+    navigator.clipboard.writeText(color15Value)
+   console.log(color15Value)
+})
+
+let color16 = document.querySelector('.col-16')
+color16.addEventListener("click",function(){
+    let color16Value = color16.querySelector('.color-16').textContent
+    navigator.clipboard.writeText(color16Value)
+   console.log(color16Value)
+})
+
+let color17 = document.querySelector('.col-17')
+color17.addEventListener("click",function(){
+    let color17Value = color17.querySelector('.color-17').textContent
+    navigator.clipboard.writeText(color17Value)
+   console.log(color17Value)
+})
+
+let color18 = document.querySelector('.col-18')
+color18.addEventListener("click",function(){
+    let color18Value = color18.querySelector('.color-18').textContent
+    navigator.clipboard.writeText(color18Value)
+   console.log(color18Value)
+})
+
+let color19 = document.querySelector('.col-19')
+color19.addEventListener("click",function(){
+    let color19Value = color19.querySelector('.color-19').textContent
+    navigator.clipboard.writeText(color19Value)
+   console.log(color19Value)
+})
+
+let color20 = document.querySelector('.col-20')
+color20.addEventListener("click",function(){
+    let color20Value = color20.querySelector('.color-20').textContent
+    navigator.clipboard.writeText(color20Value)
+   console.log(color20Value)
+})
+
+let color21 = document.querySelector('.col-21')
+color21.addEventListener("click",function(){
+    let color21Value = color21.querySelector('.color-21').textContent
+    navigator.clipboard.writeText(color21Value)
+   console.log(color21Value)
+})
+
+let color22 = document.querySelector('.col-22')
+color22.addEventListener("click",function(){
+    let color22Value = color22.querySelector('.color-22').textContent
+    navigator.clipboard.writeText(color22Value)
+   console.log(color22Value)
+})
+
+let color23 = document.querySelector('.col-23')
+color23.addEventListener("click",function(){
+    let color23Value = color23.querySelector('.color-23').textContent
+    navigator.clipboard.writeText(color23Value)
+   console.log(color23Value)
+})
+
+let color24 = document.querySelector('.col-24')
+color24.addEventListener("click",function(){
+    let color24Value = color24.querySelector('.color-24').textContent
+    navigator.clipboard.writeText(color24Value)
+   console.log(color24Value)
+})
+
+let color25 = document.querySelector('.col-25')
+color25.addEventListener("click",function(){
+    let color25Value = color25.querySelector('.color-25').textContent
+    navigator.clipboard.writeText(color25Value)
+   console.log(color25Value)
+})
+
+let color26 = document.querySelector('.col-26')
+color26.addEventListener("click",function(){
+    let color26Value = color26.querySelector('.color-26').textContent
+    navigator.clipboard.writeText(color26Value)
+   console.log(color26Value)
+})
+
+let color27 = document.querySelector('.col-27')
+color27.addEventListener("click",function(){
+    let color27Value = color27.querySelector('.color-27').textContent
+    navigator.clipboard.writeText(color27Value)
+   console.log(color27Value)
+})
+
+let color28 = document.querySelector('.col-28')
+color28.addEventListener("click",function(){
+    let color28Value = color28.querySelector('.color-28').textContent
+    navigator.clipboard.writeText(color28Value)
+   console.log(color28Value)
+})
+
+let color29 = document.querySelector('.col-29')
+color29.addEventListener("click",function(){
+    let color29Value = color29.querySelector('.color-29').textContent
+    navigator.clipboard.writeText(color29Value)
+   console.log(color29Value)
+})
+
+let color30 = document.querySelector(".col-30")
+color30.addEventListener("click", function(){
+   let color30Value = color30.querySelector(".color-30").textContent
+   navigator.clipboard.writeText(color30Value)
+   console.log(color30Value)})
+
+
+
+let color31 = document.querySelector(".col-31")
+color31.addEventListener("click", function(){
+   let color31Value = color31.querySelector(".color-31").textContent
+   navigator.clipboard.writeText(color31Value)
+   console.log(color31Value)})
+
+
+
+let color32 = document.querySelector(".col-32")
+color32.addEventListener("click", function(){
+   let color32Value = color32.querySelector(".color-32").textContent
+   navigator.clipboard.writeText(color32Value)
+   console.log(color32Value)})
+
+
+
+let color33 = document.querySelector(".col-33")
+color33.addEventListener("click", function(){
+   let color33Value = color33.querySelector(".color-33").textContent
+   navigator.clipboard.writeText(color33Value)
+   console.log(color33Value)})
+
+
+
+let color34 = document.querySelector(".col-34")
+color34.addEventListener("click", function(){
+   let color34Value = color34.querySelector(".color-34").textContent
+   navigator.clipboard.writeText(color34Value)
+   console.log(color34Value)})
+
+
+
+let color35 = document.querySelector(".col-35")
+color35.addEventListener("click", function(){
+   let color35Value = color35.querySelector(".color-35").textContent
+   navigator.clipboard.writeText(color35Value)
+   console.log(color35Value)})
+
+
+
+let color36 = document.querySelector(".col-36")
+color36.addEventListener("click", function(){
+   let color36Value = color36.querySelector(".color-36").textContent
+   navigator.clipboard.writeText(color36Value)
+   console.log(color36Value)})
+
+
+
+let color37 = document.querySelector(".col-37")
+color37.addEventListener("click", function(){
+   let color37Value = color37.querySelector(".color-37").textContent
+   navigator.clipboard.writeText(color37Value)
+   console.log(color37Value)})
+
+
+
+let color38 = document.querySelector(".col-38")
+color38.addEventListener("click", function(){
+   let color38Value = color38.querySelector(".color-38").textContent
+   navigator.clipboard.writeText(color38Value)
+   console.log(color38Value)})
+
+
+
+let color39 = document.querySelector(".col-39")
+color39.addEventListener("click", function(){
+   let color39Value = color39.querySelector(".color-39").textContent
+   navigator.clipboard.writeText(color39Value)
+   console.log(color39Value)})
+
+
+
+let color40 = document.querySelector(".col-40")
+color40.addEventListener("click", function(){
+   let color40Value = color40.querySelector(".color-40").textContent
+   navigator.clipboard.writeText(color40Value)
+   console.log(color40Value)})
+
+
+
+let color41 = document.querySelector(".col-41")
+color41.addEventListener("click", function(){
+   let color41Value = color41.querySelector(".color-41").textContent
+   navigator.clipboard.writeText(color41Value)
+   console.log(color41Value)})
